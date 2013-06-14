6c7109cccff3522a4e9262ad04d195c072f804c7
Yves Xie <yxi389@gmail.com>
2013-06-14 00:25:10 -0500

    distinguish day vs hourly stats

--- get_hourly_weather.py
+++ get_hourly_weather.py
@@ -11,7 +11,7 @@ from numpy import recfromcsv
 def readCSV(file_name):
     return recfromcsv(file_name)
 
-def RetriveWeather(unixtime):
+def RetriveWeather(unixtime,hr_or_d='hr'):
     forecast = Forecastio(config.api_key)
     result = forecast.load_forecast(config.san_diego_lat, config.san_diego_long,
                                    time=datetime.datetime.fromtimestamp(unixtime), units="si")
@@ -19,10 +19,14 @@ def RetriveWeather(unixtime):
     # use GMT 10 am every day . that is where the hourly stats seem to begin from forcast API
 
     if result['success'] is True:
-        #print "===========Hourly Data========="
-        by_hour = forecast.get_hourly()
-        #print "Hourly Summary: %s" % (by_hour.summary)
-        return by_hour.data
+        if hr_or_d == 'hr':
+            #print "===========Hourly Data========="
+            by_hour = forecast.get_hourly()
+            #print "Hourly Summary: %s" % (by_hour.summary)
+            return by_hour.data
+        else:
+            by_day = forecast.get_daily()
+            return by_day.data[0]
         #for hourly_data_point in by_hour.data:
         #    print hourly_data_point
 
@@ -41,6 +45,7 @@ if __name__ == "__main__":
     config = config.config()
     data_array = readCSV(sys.argv[1])
     weather_array = {}
+    weather_day_array = {}
     # first convert tuple to list
     data_array = [list(x) for x in data_array]
     i=2 # 3rd row is the first row that is 10am GMT
@@ -48,6 +53,9 @@ if __name__ == "__main__":
     while i < len(data_array):
         unix_time = int(data_array[i][4])
         hourly_data_points = RetriveWeather(unix_time)
+        day_data_point = RetriveWeather(unix_time,'d')
+        weather_day_array[day_data_point.unixtime] = day_data_point
+        print len(hourly_data_points)
         for point in hourly_data_points:
             weather_array[point.unixtime] = point
         i += 24 # each call to the API cantains 24 hourly stats
@@ -55,16 +63,17 @@ if __name__ == "__main__":
     for record in data_array:
         try:
             data_point = weather_array[int(record[4])]
-            between_sun = data_point.sunsetTime -data_point.sunriseTime
+            day_data_point = weather_day_array[int(record[4])]
+            between_sun = day_data_point.sunsetTime -day_data_point.sunriseTime
             precipIntensity = data_point.precipIntensity
-            precipIntensityMax = data_point.precipIntensityMax
+            precipIntensityMax = day_data_point.precipIntensityMax
             #precipIntensityMaxTime
-            precipProbability = data_point.precipProbability
-            precipType = data_point.precipType
-            precipAccumulation = data_point.precipAccumulation
+            #precipProbability = day_data_point.precipProbability
+            #precipType = day_data_point.precipType
+            #precipAccumulation = day_data_point.precipAccumulation
             temperature = data_point.temperature
-            temperature_delta = data_point.temperatureMax - data_point.temperatureMin
-            temperature_min_to_max = data_point.temperatureMinTime - data_point.temperatureMaxTime
+            temperature_delta = day_data_point.temperatureMax - day_data_point.temperatureMin
+            temperature_min_to_max = day_data_point.temperatureMinTime - day_data_point.temperatureMaxTime
             dewPoint = data_point.dewPoint
             windspeed = data_point.windspeed
             windbaring = data_point.windbaring
@@ -74,10 +83,10 @@ if __name__ == "__main__":
             visbility = data_point.visbility
             #ozone = data_point.ozone
 
-            record.extend([between_sun,precipIntensity,precipIntensityMax,precipProbability,precipType,precipAccumulation,
+            record.extend([between_sun,precipIntensity,precipIntensityMax,
                            temperature,temperature_delta,temperature_min_to_max,dewPoint,windspeed,windbaring,cloudcover,
                            humidity,pressure,visbility])
         except:
             print sys.exc_info()
 
-    print json.dumps(data_array)
+    print data_array
